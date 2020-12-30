8ab8f21f30abb67681676d3ccf772a2365b5b483
Vasily Boltunov <debugger.i.am@mail.ru>
2020-12-30 18:46:10 +0300

    Setup block to tamplate view. Begun room option block

--- src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,7 +1,15 @@
 $(()=> {
-  $('.expandable-checkbox-list__title').on('click','.field-template', function(event) {
-    let idToSearch = $(event.delegateTarget).siblings('fieldset');
-    console.log(idToSearch);
-    idToSearch.toggle('.expandable-checkbox-list__fieldset--hidden');
-  });
+	$('.expandable-checkbox-list__title').on('click','.field-template', function(event) {
+		let idToSearch = $(event.delegateTarget).siblings('fieldset');
+		let arrowContainer = $(this).find('.field-template__arrow-css')
+		if ( arrowContainer.hasClass('field-template__arrow-css--to-top') ){
+			arrowContainer.removeClass('field-template__arrow-css--to-top');
+			arrowContainer.addClass('field-template__arrow-css--to-bottom');
+		} else {
+			arrowContainer.removeClass('field-template__arrow-css--to-bottom');
+			arrowContainer.addClass('field-template__arrow-css--to-top');
+		}
+		
+		idToSearch.toggle('.expandable-checkbox-list__fieldset--hidden');
+	});
 })

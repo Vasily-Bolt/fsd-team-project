f50a358d2c3c14d2a219b8664947ac86e9112451
Vasily Boltunov <debugger.i.am@mail.ru>
2021-01-01 22:03:17 +0300

    Setup Russian language (datepicker+moment.js)

--- src/assets/blocks/datepicker/datepicker.js
+++ src/assets/blocks/datepicker/datepicker.js
@@ -1,14 +1,21 @@
 $(()=> {
+
 	let trigger = 'open';
 	$( 'div[id^="datepicker"]' ).each( function() {
 		$(this).find('input[id$="InputField"]').dateRangePicker(
 			{
-				format: 'DD.MM.YYYY',
-				autoClose: false,
-				singleDate : true,
+				format: 'DD.MMM',
+				singleMonth: true,
+				language: 'ru',
+				singleDate : false,
 				showShortcuts: false,
+				showTopbar: true,
+				autoClose: false,
 			});
 	});
+
+//Сделай если закрывается кнопкой (из самого пикера), то надо менять триггер! В плагине есть такая функция
+
 	$( 'div[id^="datepicker"]' ).click( function(event) {
 		event.stopPropagation();
 		if (trigger == 'open') {

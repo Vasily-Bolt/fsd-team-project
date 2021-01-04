2da592175680998cb85f0bd41de397bf0958e368
Vasily Boltunov <debugger.i.am@mail.ru>
2021-01-04 15:06:34 +0300

    Made datepicker pug file. Fixed some bugs to fit template

--- src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -1,4 +1,5 @@
 $(()=> {
+	$('.expandable-checkbox-list__title').find('input').addClass('header3');
 	$('.expandable-checkbox-list__title').on('click','.field-template', function(event) {
 		let idToSearch = $(event.delegateTarget).siblings('fieldset');
 		let arrowContainer = $(this).find('.field-template__arrow-css')

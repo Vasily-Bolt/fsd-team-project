1c6a760b2713519ddf4f2ef6f2668e1feaf70f30
Болтунов Василий Витальевич <C:\MAIL>
2021-01-14 10:39:38 +0300

    Algorythm of price calculation finished. Need to change word depending on quantity of days

--- src/assets/blocks/room-booking/room-booking.js
+++ src/assets/blocks/room-booking/room-booking.js
@@ -8,42 +8,36 @@ function integerToStringMyFormat ( valueToConvert ) {
 $(()=> {
 	$('.room-booking').each( function() {
 		//Объект всех переменных нужных для подсчета. Содержит метод для расчета
-		// let roomBookingObject = {
-		// 	blockWithNumberOfDays = $(this).find('span[id$="daysOfResidence"]'),
-		// 	pricePerDay = $(this).find('span[id$="pricePerDay"]').attr('data-value'),
-		// 	discount = $(this).find('span[id$="discount"]').attr('data-value'),
-		// 	servicesPrice = $(this).find('span[id$="servicesPrice"]').attr('data-value'),
-		// 	extraServices = $(this).find('span[id$="extraServices"]').attr('data-value'),
-		// 	pricePerPeriod = $(this).find('span[id$="pricePerPeriod"]'),
-		// 	totalPrice = $(this).find('span[id$="totalPrice"]'),
-		// 	datePickerDataSelector = $(this).find('input[id$="InputField"]').data('dateRangePicker'),
-		// };
+		let roomBookingObject = {
+			blockWithNumberOfDays: 	$(this).find('span[id$="daysOfResidence"]'),//Для быстрой смены цифры количества суток
+			pricePerDay: 						$(this).find('span[id$="pricePerDay"]').attr('data-value'),//Со значением суточной стоимости
+			discount: 							$(this).find('span[id$="discount"]').attr('data-value'),//Со значением скидки
+			servicesPrice: 					$(this).find('span[id$="servicesPrice"]').attr('data-value'),//Со значением сбора за услуги
+			extraServices: 					$(this).find('span[id$="extraServices"]').attr('data-value'),//Со значением сбора дополнительные за услуги
+			pricePerPeriod: 				$(this).find('span[id$="pricePerPeriod"]'),//Для быстрого доступа к полю с суммой за проживание
+			totalPrice: 						$(this).find('span[id$="totalPrice"]'),//Для быстрого доступа к полю с общей суммой
+			datePickerDataSelector:	$(this).find('input[id$="InputField"]').data('dateRangePicker'),//Для быстрого доступа к Пикеру
+			daysOfResidence:				function() {
+				//через метод getDaysPicked плагина datepickerselector получаем количество дней пребывания и сохраняем в переменную
+				let howMuchDays = this.datePickerDataSelector.getDaysPicked();
+				if ( howMuchDays == 1) return howMuchDays;
+				else return howMuchDays - 1;
+			},
+			recalculateValues:							function () {
+				howMuchDays = this.daysOfResidence();
+				this.blockWithNumberOfDays.html( howMuchDays );
+				let total = howMuchDays*this.pricePerDay
 
-		//Переменная для быстрой смены цифры количества суток		
-		let blockWithNumberOfDays = $(this).find('span[id$="daysOfResidence"]');
-		
-		//Переменная со значением суточной стоимости
-		let pricePerDay = $(this).find('span[id$="pricePerDay"]').attr('data-value');
+				this.pricePerPeriod.html( integerToStringMyFormat( total ) + currency );
 
-		//Переменная со значением скидки
-		let discount = $(this).find('span[id$="discount"]').attr('data-value');
-		
-		//Переменная со значением сбора за услуги
-		let servicesPrice = $(this).find('span[id$="servicesPrice"]').attr('data-value');
+				if ( this.discount ) total -= +this.discount;
+				if ( this.servicesPrice ) total += +this.servicesPrice;
+				if ( this.extraServices ) total += +this.extraServices;
 
-		//Переменная со значением сбора дополнительные за услуги
-		let extraServices = $(this).find('span[id$="extraServices"]').attr('data-value');
+				this.totalPrice.html( integerToStringMyFormat( total + currency ) );
+			}
+		};
 
-		//Для быстрого доступа к полю с суммой за проживание
-		let pricePerPeriod = $(this).find('span[id$="pricePerPeriod"]');
-		
-		////Для быстрого доступа к полю с общей суммой
-		let totalPrice = $(this).find('span[id$="totalPrice"]');
-
-		//Делаем переменную для быстрого доступа к Пикеру
-		let datePickerDataSelector = $(this).find('input[id$="InputField"]').data('dateRangePicker');
-
-		
 		//При рендеринге страницы поля заполняются заданными датами, но плагин до первого вызова
 		//не подтягивает их и метод getDaysPicked не работает. Потому выковыриваем информацию из
 		//ДОМ и после кидаем через метод SetDateRange
@@ -51,34 +45,14 @@ $(()=> {
 			$(this).find('input[id$="multiple-firstInputField"]').val(),
 			$(this).find('input[id$="multiple-secondInputField"]').val()
 		];
-		datePickerDataSelector.setDateRange ( ResidenceDaysRange[0], ResidenceDaysRange[1] );
-
-		//Теперь через метод getDaysPicked получаем количество дней пребывания и сохраняем в переменную
-		let daysOfResidence = datePickerDataSelector.getDaysPicked()-1;
-		blockWithNumberOfDays.html( daysOfResidence );
-		
-		let countedTotalPrice = daysOfResidence*pricePerDay;
-
-		pricePerPeriod.html( integerToStringMyFormat( countedTotalPrice ) + currency );
-		if ( discount ) countedTotalPrice -= +discount;
-		if ( servicesPrice ) countedTotalPrice += +servicesPrice;
-		if ( extraServices ) countedTotalPrice += +extraServices;
-		totalPrice.html( integerToStringMyFormat( countedTotalPrice + currency ) );
+		roomBookingObject.datePickerDataSelector.setDateRange ( ResidenceDaysRange[0], ResidenceDaysRange[1] );
 
 		//Устанавливаем слушатель на trigger datepicker-date-changed. При срабатывании переписываем в DOM
 		//количество суток
+		roomBookingObject.recalculateValues();
+
 		$(this).find('div[id^="datepickerHead"').on('datepicker-date-changed', function(){
-			daysOfResidence = $(this).attr('data-days');
-			blockWithNumberOfDays.html( daysOfResidence );
-			
-			countedTotalPrice = daysOfResidence*pricePerDay;
-
-			pricePerPeriod.html( integerToStringMyFormat( countedTotalPrice ) + currency );
-			if ( discount ) countedTotalPrice -= +discount;
-			if ( servicesPrice ) countedTotalPrice += +servicesPrice;
-			if ( extraServices ) countedTotalPrice += +extraServices;
-			totalPrice.html( integerToStringMyFormat( countedTotalPrice + currency ) );
-
+			roomBookingObject.recalculateValues();
 		}); 
 
 

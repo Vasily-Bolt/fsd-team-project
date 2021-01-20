e1ab3b412572b2a33703a17c791e8f5cc73fa960
Болтунов Василий Витальевич <C:\MAIL>
2021-01-20 18:13:07 +0300

    Triangle arrows cut out to seperate block

--- src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
+++ src/assets/blocks/expandable-checkbox-list/expandable-checkbox-list.js
@@ -2,13 +2,13 @@ $(()=> {
 	$('.expandable-checkbox-list__title').find('input').addClass('header3');
 	$('.expandable-checkbox-list__title').on('click','.field-template', function(event) {
 		let idToSearch = $(event.delegateTarget).siblings('fieldset');
-		let arrowContainer = $(this).find('.field-template__arrow-css')
-		if ( arrowContainer.hasClass('field-template__arrow-css--to-top') ){
-			arrowContainer.removeClass('field-template__arrow-css--to-top');
-			arrowContainer.addClass('field-template__arrow-css--to-bottom');
+		let arrowContainer = $(this).find('.arrow-triangle')
+		if ( arrowContainer.hasClass('arrow-triangle--to-top') ){
+			arrowContainer.removeClass('arrow-triangle--to-top');
+			arrowContainer.addClass('arrow-triangle--to-bottom');
 		} else {
-			arrowContainer.removeClass('field-template__arrow-css--to-bottom');
-			arrowContainer.addClass('field-template__arrow-css--to-top');
+			arrowContainer.removeClass('arrow-triangle--to-bottom');
+			arrowContainer.addClass('arrow-triangle--to-top');
 		}
 		
 		idToSearch.toggle('.expandable-checkbox-list__fieldset--hidden');

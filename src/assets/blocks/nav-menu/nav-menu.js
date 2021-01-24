0fbb73eee1a9c08c2aac6bd02ce88bbf516d3582
Vasily Boltunov <debugger.i.am@mail.ru>
2021-01-24 21:45:42 +0300

    Still working on logics ;)

--- src/assets/blocks/nav-menu/nav-menu.js
+++ src/assets/blocks/nav-menu/nav-menu.js
@@ -2,11 +2,18 @@ $(()=> {
 
 	$( '.nav-menu__arrow-container' ).parent().on( 'click', function(){
 		$(this).children('.nav-menu__submenu').toggleClass('nav-menu__submenu--hidden nav-menu__submenu--visible	');
-		
-		$(document).one('click', function() {
-			console.log('321');	
-			$('.nav-menu__submenu').removeClass('nav-menu__submenu--hidden nav-menu__submenu--visible	');
-		});
 	});
-	
+
+	// $('body').on('click', function( event ) {
+	// 	console.log('doit');
+	// 	// $( '.nav-menu__submenu' )
+	// 	// .hasClass( 'nav-menu__submenu--visible' )
+	// 	// .removeClass( 'nav-menu__submenu--visible' )
+	// 	// .addClass( 'nav-menu__submenu--hidden' );
+	// 	if ( $( '.nav-menu__submenu' ).hasClass( 'nav-menu__submenu--visible' ) ) {
+	// 		$( '.nav-menu__submenu' ).removeClass( 'nav-menu__submenu--visible' ).addClass( 'nav-menu__submenu--hidden' );
+	// 	}
+	// });
+
+
 });
